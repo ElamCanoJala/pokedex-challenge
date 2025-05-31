880b3ece3b648a2a9bdc72e105e988e1026bd793
Elam Cano <elamcano16@gmail.com>
2025-05-31 18:47:41 -0300

    pokemon box responsive try

--- src/components/organisms/Pokemon.Card.jsx
+++ src/components/organisms/Pokemon.Card.jsx
@@ -1,12 +1,19 @@
 import PokemonImage from "../atoms/PokemonImage";
 import PokemonInfo from "../molecules/PokemonInfo";
 import styles from "./PokemonCard.module.css";
+import pattern from "../../assets/card-components/Pattern.svg";
+import pokeball from "../../assets/card-components/Pokeball.svg";
 
 function PokemonCard({ pokemon }) {
   return (
     <div className={styles.card}>
-      <PokemonInfo pokemon={pokemon} />
+      <div className={styles.info}>
+        <img src={pattern} />
+        <PokemonInfo pokemon={pokemon} />
+      </div>
+
       <div className={styles.image_container}>
+        <img src={pokeball} className={styles.bg_image} />
         <PokemonImage image={pokemon.image} alt={pokemon.name} />
       </div>
     </div>
