8edcce9c0777d76ab8f3e256f8cb6ac9f59ea97e
Elam Cano <elamcano16@gmail.com>
2025-05-31 14:59:10 -0300

    add svg to assets

--- src/utils/pokemonTypeStyles.js
+++ src/utils/pokemonTypeStyles.js
@@ -1,35 +1,95 @@
-import grass from "../assets/types/grass.svg";
-import poison from "../assets/types/poison.svg";
-// import water from "../assets/types/water.svg";
-// import bug from "../assets/types/bug.svg";
-// import fire from "../assets/types/fire.svg";
-// import electric from "../assets/types/electric.svg";
+import normal from "../assets/types/Normal.svg";
+import fire from "../assets/types/Fire.svg";
+import water from "../assets/types/Water.svg";
+import electric from "../assets/types/Electric.svg";
+import grass from "../assets/types/Grass.svg";
+import ice from "../assets/types/Ice.svg";
+import fighting from "../assets/types/Fighting.svg";
+import poison from "../assets/types/Poison.svg";
+import ground from "../assets/types/Ground.svg";
+import flying from "../assets/types/Flying.svg";
+import psychic from "../assets/types/Psychic.svg";
+import bug from "../assets/types/Bug.svg";
+import rock from "../assets/types/Rock.svg";
+import ghost from "../assets/types/Ghost.svg";
+import dragon from "../assets/types/Dragon.svg";
+import dark from "../assets/types/Dark.svg";
+import steel from "../assets/types/Steel.svg";
+import fairy from "../assets/types/Fairy.svg";
 
 const typeStyles = {
-  grass: {
-    color: "#78C850",
-    image: grass,
+  normal: {
+    color: "#A8A77A",
+    image: normal,
   },
-  poison: {
-    color: "#A040A0",
-    image: poison,
-  },
-  /* fire: {
-    color: "#F08030",
+  fire: {
+    color: "#EE8130",
     image: fire,
   },
   water: {
-    color: "#6890F0",
+    color: "#6390F0",
     image: water,
   },
+  electric: {
+    color: "#F7D02C",
+    image: electric,
+  },
+  grass: {
+    color: "#7AC74C",
+    image: grass,
+  },
+  ice: {
+    color: "#96D9D6",
+    image: ice,
+  },
+  fighting: {
+    color: "#C22E28",
+    image: fighting,
+  },
+  poison: {
+    color: "#A33EA1",
+    image: poison,
+  },
+  ground: {
+    color: "#E2BF65",
+    image: ground,
+  },
+  flying: {
+    color: "#A98FF3",
+    image: flying,
+  },
+  psychic: {
+    color: "#F95587",
+    image: psychic,
+  },
   bug: {
-    color: "#A8B820",
+    color: "#A6B91A",
     image: bug,
   },
-  electric: {
-    color: "#F8D030",
-    image: electric,
-  }, */
+  rock: {
+    color: "#B6A136",
+    image: rock,
+  },
+  ghost: {
+    color: "#735797",
+    image: ghost,
+  },
+  dragon: {
+    color: "#6F35FC",
+    image: dragon,
+  },
+  dark: {
+    color: "#705746",
+    image: dark,
+  },
+  steel: {
+    color: "#B7B7CE",
+    image: steel,
+  },
+  fairy: {
+    color: "#D685AD",
+    image: fairy,
+  },
 };
 
 export default typeStyles;
