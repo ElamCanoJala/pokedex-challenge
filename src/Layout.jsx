e3fcc26ad9c3e38f30ccc4d3637aea81f33afe06
Elam Cano <elamcano16@gmail.com>
2025-07-22 18:45:49 -0300

    add lazy to routes and change layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,14 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "./components/organisms/Navbar.jsx";
 import Footer from "./components/atoms/Footer.jsx";
 import "./Layout.css";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div className="layout-grid">
       <header className="header">
         <Navbar />
       </header>
-      <main className="main">
-        <Outlet />
-      </main>
+      <main className="main">{children}</main>
       <footer className="footer">
         <Footer />
       </footer>
--- src/router.jsx
+++ src/router.jsx
@@ -1,42 +1,65 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Suspense } from "react";
-
-import Layout from "./Layout.jsx";
-import Pokemons from "./pages/Pokemons.jsx";
-import Game from "./pages/Game.jsx";
-import ErrorPage from "./pages/ErrorPage.jsx";
-import Loading from "./components/atoms/Loading.jsx";
-import PokemonDetail from "./pages/PokemonDetail.jsx";
-import { pokemonsLoader } from "./loaders/pokemonsLoader.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <Suspense fallback={<Loading />}>
-        <Layout />
-      </Suspense>
-    ),
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Pokemons />,
+    lazy: async () => {
+      const [{ default: Layout }, { default: Pokemons }, { pokemonsLoader }] =
+        await Promise.all([
+          import("./Layout.jsx"),
+          import("./pages/Pokemons.jsx"),
+          import("./loaders/pokemonsLoader.js"),
+        ]);
+
+      return {
+        element: (
+          <Layout>
+            <Pokemons />
+          </Layout>
+        ),
         loader: pokemonsLoader,
-      },
-      {
-        path: "game",
-        element: <Game />,
-      },
-      {
-        path: "pokemon/:number",
-        element: <PokemonDetail />,
-      },
-    ],
+        errorElement: <div>Ocurrió un error al cargar la página</div>,
+      };
+    },
+  },
+  {
+    path: "/game",
+    lazy: async () => {
+      const [{ default: Layout }, { default: Game }] = await Promise.all([
+        import("./Layout.jsx"),
+        import("./pages/Game.jsx"),
+      ]);
+
+      return {
+        element: (
+          <Layout>
+            <Game />
+          </Layout>
+        ),
+      };
+    },
+  },
+  {
+    path: "/pokemon/:number",
+    lazy: async () => {
+      const [{ default: Layout }, { default: PokemonDetail }] =
+        await Promise.all([
+          import("./Layout.jsx"),
+          import("./pages/PokemonDetail.jsx"),
+        ]);
+
+      return {
+        element: (
+          <Layout>
+            <PokemonDetail />
+          </Layout>
+        ),
+      };
+    },
   },
   {
     path: "*",
-    element: <ErrorPage />,
+    element: (await import("./pages/ErrorPage.jsx")).default(),
   },
 ]);
 
